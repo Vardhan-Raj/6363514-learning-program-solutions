8c52491cae64343fd531a92ba405a15e25c6d126
Vardhan-Raj <22052843@kiit.ac.in>
2025-06-22 17:01:03 +0530

    bin and obj auto update

--- Week 1/SingletonPattern/obj/Debug/net8.0/SingletonPattern.AssemblyInfo.cs
+++ Week 1/SingletonPattern/obj/Debug/net8.0/SingletonPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e86f347ef3bfacea11702885f72dd0654493db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c0a3256d25187dc70cce54c1ecd4de911197d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
