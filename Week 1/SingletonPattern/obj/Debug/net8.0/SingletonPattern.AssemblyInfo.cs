0ca109bddcc68daf4aa5a0c514c42612cdc2768b
Vardhan-Raj <22052843@kiit.ac.in>
2025-06-29 19:04:14 +0530

    just update

--- Week 1/SingletonPattern/obj/Debug/net8.0/SingletonPattern.AssemblyInfo.cs
+++ Week 1/SingletonPattern/obj/Debug/net8.0/SingletonPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c0a3256d25187dc70cce54c1ecd4de911197d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0eb20e8f4e39c124470fed565cdf63b943ee96")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
