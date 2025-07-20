f5fb76df26886eca980821543a46f0e9899eddb7
Vardhan-Raj <22052843@kiit.ac.in>
2025-07-20 20:13:41 +0530

    HOL4 completed

---
+++ Week 5/blogapp/src/Posts.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Post from "./Post";
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false,
+    };
+  }
+
+  loadPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data }))
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("Something went wrong: " + error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Error loading posts.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>All Posts</h1>
+        {this.state.posts.map((post) => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
